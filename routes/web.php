084ef5d93887eb28b93d214e2e73af91c2ed13d2
ridwanul <ridwanul.riseuplabs@gmail.com>
2023-01-26 17:53:09 +0600

    Created: registration, login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::group(['middlewire' => 'auth'], function(){
+    Route::get('tt', TestController::class)->middleware('can:dash_view')->name('tt');
+// });
